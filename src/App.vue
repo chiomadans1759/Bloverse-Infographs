e366cf35f869070dbebb1e47bd377ac41c2df20f
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2019-02-11 13:01:42 +0100

    Added necessary font for project

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,13 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat|Open+Sans');
+
+#app {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
+
+
