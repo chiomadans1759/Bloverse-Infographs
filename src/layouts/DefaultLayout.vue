eff9d6df317c6e6b805e5e81076357834cb9568d
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2019-02-11 11:26:15 +0100

    Edited dafualt layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="default-layout">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default-layout"
+}
+</script>
+
+<style scoped>
+</style>
