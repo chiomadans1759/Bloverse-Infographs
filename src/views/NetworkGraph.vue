8611defc7d35beff6656f54db61aee5898176d31
Chiomadans1759 <chiomadans@gmail.com>
2019-02-12 11:42:44 +0100

    Created the Header Component

---
+++ src/views/NetworkGraph.vue
@@ -0,0 +1,16 @@
+<template>
+    <div> 
+        <Header/>
+        <Networks/>  
+    </div>
+</template>
+
+
+<script>
+import Header from "@/components/Header.vue";
+import Networks from "@/components/Networks.vue";
+export default {
+  name: "network",
+  components: {Header, Networks}
+};
+</script>
