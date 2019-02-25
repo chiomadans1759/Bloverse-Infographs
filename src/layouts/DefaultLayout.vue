cedcebdb2b75a196cc467a6c6d50c39665afffde
filigreti <aolfiligre@gmail.com>
2019-02-25 16:59:15 +0100

    created intro page

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="default-layout">
-    <default-header />
-    <router-view />
+    <intro />
+    <trending />
   </div>
 </template>
 
 <script>
+import intro from "@/views/intro.vue";
+import trending from "@/views/trending.vue"
 export default {
   name: "default-layout",
-}
+  components: {
+    intro,
+    trending
+  }
+};
 </script>
+<style>
+#default-layout{
+  overflow-x:hidden
+}
+</style>
---
+++ src/views/trending.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="trending container mt-5">
+        <h2>Trending</h2>
+        <h4>See the most popular keywords used on our platform</h4>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
